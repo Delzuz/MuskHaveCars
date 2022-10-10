0f5a719d59fc288733222cf00dc2dcac16ccf7c8
Harris Novljakovic <harrisnovljakovic@gmail.com>
2022-10-10 13:03:11 +0200

    Initial commit

---
+++ src/main/java/com/example/MuskHaveCars/MuskHaveCarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MuskHaveCars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuskHaveCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuskHaveCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MuskHaveCars/MuskHaveCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MuskHaveCars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuskHaveCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
