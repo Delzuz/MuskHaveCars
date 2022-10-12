a5d38d336d5ae53e1231e2bc47c89f8e1122a190
Magnus <magnusgronhult@gmail.com>
2022-10-12 16:49:01 +0200

    local commit

--- src/main/java/com/example/MuskHaveCars/Controller/MuskController.java
+++ src/main/java/com/example/MuskHaveCars/Controller/MuskController.java
@@ -4,8 +4,11 @@ import com.example.MuskHaveCars.Classes.Car;
 import com.example.MuskHaveCars.Repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,7 +20,12 @@ public class MuskController {
     @GetMapping("/cars")
     public List<Car> cars() {
         List<Car> cars = (List<Car>) carRepository.findAll();
+
         return cars;
+    }
+    @RequestMapping("/hello")
+    void handleThis(HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://stackoverflow.com/questions/29085295/spring-mvc-restcontroller-and-redirect");
     }
 
 
